f2c4c1e76d8c9cfe43829acc4a35a0cb1484b325
Szymon Florek <szymon.florek@student.wat.edu.pl>
2024-09-28 03:01:27 +0200

    logger

--- src/utils/custom_logger.py
+++ src/utils/custom_logger.py
@@ -3,19 +3,25 @@ import os
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
 import pytz
-import colorama
-from colorama import Fore, Style
-
-# Inicjalizacja Colorama z force=True dla Railway
-colorama.init(autoreset=True, strip=False, convert=True)
+from rich.logging import RichHandler
+from rich.console import Console
+from rich.theme import Theme
 
 # Definicja strefy czasowej dla Polski
 POLAND_TZ = pytz.timezone('Europe/Warsaw')
 
-class PolandFormatter(logging.Formatter):
-    def __init__(self, fmt=None, datefmt=None, style='{'):
-        super().__init__(fmt, datefmt, style)
+custom_theme = Theme({
+    "info": "green",
+    "warning": "yellow",
+    "error": "red",
+    "critical": "bold red",
+    "debug": "blue",
+})
 
+console = Console(theme=custom_theme)
+
+
+class PolandFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
         dt = datetime.fromtimestamp(record.created, POLAND_TZ)
         if datefmt:
@@ -24,99 +30,36 @@ class PolandFormatter(logging.Formatter):
             s = dt.isoformat(timespec='milliseconds')
         return s
 
-class ColorFormatter(logging.Formatter):
-    # # Format logu z różnymi sekcjami dla lepszej czytelności
-    # FORMAT = ("{time_color}{asctime}{reset} [{level_color}{levelname}{reset}] {name_color}{module}:{funcName}:{"
-    #           "lineno}{reset} - {message_color}{message}{reset}")
-    #
-    # # Definicje kolorów dla różnych części logu
-    # COLOR_CODES = {
-    #     'TIME': Fore.CYAN,
-    #     'LEVEL': {
-    #         logging.DEBUG: Fore.BLUE,
-    #         logging.INFO: Fore.GREEN,
-    #         logging.WARNING: Fore.YELLOW,
-    #         logging.ERROR: Fore.RED,
-    #         logging.CRITICAL: Fore.MAGENTA,
-    #     },
-    #     'NAME': Fore.WHITE,
-    #     'MESSAGE': {
-    #         logging.DEBUG: Fore.LIGHTBLUE_EX,
-    #         logging.INFO: Fore.LIGHTWHITE_EX,
-    #         logging.WARNING: Fore.LIGHTYELLOW_EX,
-    #         logging.ERROR: Fore.LIGHTRED_EX,
-    #         logging.CRITICAL: Fore.LIGHTMAGENTA_EX,
-    #     },
-    #     'RESET': Style.RESET_ALL
-    # }
-    #
-    # def format(self, record):
-    #     time_color = self.COLOR_CODES['TIME']
-    #     level_color = self.COLOR_CODES['LEVEL'].get(record.levelno, Fore.WHITE)
-    #     name_color = self.COLOR_CODES['NAME']
-    #     message_color = self.COLOR_CODES['MESSAGE'].get(record.levelno, Fore.WHITE)
-    #     reset = self.COLOR_CODES['RESET']
-    #
-    #     log_fmt = self.FORMAT.format(
-    #         time_color=time_color,
-    #         asctime="[{asctime}]",
-    #         reset=reset,
-    #         level_color=level_color,
-    #         levelname="{levelname}",
-    #         name_color=name_color,
-    #         module="{module}",
-    #         funcName="{funcName}",
-    #         lineno="{lineno}",
-    #         message_color=message_color,
-    #         message="{message}"
-    #     )
-    #
-    #     formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S", style='{')
-    #     return formatter.format(record)
-    # Format logu z różnymi sekcjami dla lepszej czytelności
-    FORMAT = ("\033[36m{asctime}\033[0m [{levelname}] {module}:{funcName}:{lineno} - {message}")
-
-    # Definicje kolorów dla różnych poziomów logowania
-    COLORS = {
-        logging.DEBUG: "\033[34m",  # Blue
-        logging.INFO: "\033[32m",  # Green
-        logging.WARNING: "\033[33m",  # Yellow
-        logging.ERROR: "\033[31m",  # Red
-        logging.CRITICAL: "\033[35m",  # Magenta
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMAT
-        levelname = self.COLORS.get(record.levelno, "\033[37m") + record.levelname + "\033[0m"
-        log_fmt = log_fmt.replace("{levelname}", levelname)
-        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S", style='{')
-        return formatter.format(record)
 
 def setup_custom_logger(name, log_file, level=logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    poland_formatter = PolandFormatter('{asctime} {levelname} {module}:{funcName}:{lineno} - {message}',
-                                       datefmt='%Y-%m-%d %H:%M:%S %Z')
+    # Formatter dla pliku
+    file_formatter = PolandFormatter('{asctime} {levelname} {module}:{funcName}:{lineno} - {message}',
+                                     datefmt='%Y-%m-%d %H:%M:%S %Z', style='{')
 
+    # Handler dla pliku
     file_handler = RotatingFileHandler(log_file, maxBytes=10 * 1024 * 1024, backupCount=5)
-    file_handler.setFormatter(poland_formatter)
+    file_handler.setFormatter(file_formatter)
     file_handler.setLevel(level)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(ColorFormatter())
+    # Rich handler dla konsoli
+    console_handler = RichHandler(console=console, rich_tracebacks=True)
     console_handler.setLevel(level)
 
+    # Dodanie handlerów do loggera
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     return logger
 
+
 # Upewnij się, że katalog logów istnieje
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
-# Stwórz loggery z nowym formatowaniem
+# Stwórz loggery
 main_logger = setup_custom_logger('[Main]', os.path.join(log_dir, 'main.log'))
 scheduler_logger = setup_custom_logger('[Scheduler]', os.path.join(log_dir, 'scheduler.log'))
 google_api_logger = setup_custom_logger('[GoogleAPI]', os.path.join(log_dir, 'google_api.log'))
